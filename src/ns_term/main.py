ddd64b057bb7f2f2ae91bfbd6f3dbf2d3cfd9438
jowens25 <jowens@novuspower.com>
2026-07-02 10:02:18 -0500

    added read reg

--- src/ns_term/main.py
+++ src/ns_term/main.py
@@ -2,6 +2,7 @@ import importlib.metadata
 
 import typer
 import serial.tools.list_ports
+from serial import Serial
 
 import sys
 app = typer.Typer()
@@ -29,6 +30,96 @@ def listen(_port, _baud):
 
     while(True):
         print(ser.readline().decode(encoding="utf-8", errors="ignore"), end="")
+
+
+def read_reg(ser: Serial, addr: int, data: list[int]) -> str:
+    write_data = ""
+    write_data += "$RC,"
+    write_data += f"{addr:#010x}"
+
+    cs = 0
+    for a in write_data:
+        if a != "$":
+            cs = cs ^ ord(a)
+    write_data += "*"
+    write_data += f"{cs:02x}"
+    write_data += "\r\n"
+
+    #print(f"write data: {write_data}")
+
+    ser.write(write_data.encode(encoding="utf-8", errors="ignore"))
+
+    rsp = ser.readline().decode(encoding="utf-8", errors="ignore")
+
+    if rsp:
+        if rsp.startswith("$RR"):
+            data[0] = int(rsp.split("*")[0].split(",")[2], 16)
+            return 0
+        else:
+            return -1
+
+
+Ucm_Config_BlockSize = 16
+Ucm_Config_TypeInstanceReg = 0x00000000
+Ucm_Config_BaseAddrLReg =                          0x00000004
+Ucm_Config_BaseAddrHReg =                          0x00000008
+Ucm_Config_IrqMaskReg =                            0x0000000C
+Ucm_CoreConfig_ConfSlaveCoreType = 1
+
+def read_cores(ser):
+
+    temp_data = [0]
+
+    for i in range(255):
+        if (0 == read_reg(ser, (0x00000000 + ((i * Ucm_Config_BlockSize) + Ucm_Config_TypeInstanceReg)), temp_data)):
+
+            if ((i == 0) and ((((temp_data[0] >> 16) & 0x0000FFFF) != Ucm_CoreConfig_ConfSlaveCoreType) or (((temp_data[0] >> 0) & 0x0000FFFF) != 1))):
+                print("ERROR: not a conf block at the address expected")
+                break
+                
+            elif (temp_data[0] == 0):
+                break
+            else:
+                print(f"core_type: { ((temp_data[0] >> 16) & 0x0000FFFF)}")
+                print(f"core_instance_nr: { ((temp_data[0] >> 0) & 0x0000FFFF)}")
+
+        else:
+            break
+
+
+        if (0 == read_reg(ser, (0x00000000 + ((i * Ucm_Config_BlockSize) + Ucm_Config_BaseAddrLReg)), temp_data)):
+        
+            print(f"address_range_low: { temp_data[0]:#010x}")
+        
+        else:
+        
+            break
+            
+
+        if (0 == read_reg(ser, (0x00000000 + ((i * Ucm_Config_BlockSize) + Ucm_Config_BaseAddrHReg)), temp_data)):
+            print(f"address_range_high: {temp_data[0]:#010x}")
+        else:
+            break
+            
+
+        if (0 == read_reg(ser, (0x00000000 + ((i * Ucm_Config_BlockSize) + Ucm_Config_IrqMaskReg)), temp_data)):
+            print(f"interrupt_mask: { temp_data[0]:#010x}")
+        else:
+            break
+                            
+
+
+
+@app.command()
+def cores(port, baud):
+
+    ser = serial.Serial(port, baudrate=baud, timeout=1)
+
+    ser.write("$CC\r\n".encode(encoding="utf-8", errors="ignore"))
+
+    print(ser.readline())
+
+    read_cores(ser)
 
 
 @app.command()
